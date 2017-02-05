40862858c8e1f673359a7a8497acfb0fc9c70bc1
milannankov <public@nankov.com>
2017-02-05 13:08:55 +0200

    Initial commit

---
+++ source/BlobStorageHandsOn/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace BlobStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 01 - Connect to your azure storage account
+
+            // 02 - Create a container called "text-files"
+
+            // 03 - Set the container permissions to BlobContainerPublicAccessType.Blob
+
+            // 04 - Upload SampleText.txt to a block block called "UploadedSampleText.txt"
+
+            // 05 - Download "UploadedSampleText.txt" from the storage account and prints its contents using Console.WriteLine()
+
+            // 05 - Delete UploadedSampleText.txt from the storage
+        }
+    }
+}
---
+++ source/TableStorageHandsOn/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Configuration;
+
+namespace TableStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 01 - Connect to your azure storage account
+
+            // 02 - Create a table called "customers"
+
+            // 03 - Insert single entity (instance of the CustomerEntity class) into the table
+
+            // 04 - Insert two additional CustomerEntity objects using batching (use PartitionKey "Netherlands")
+
+            // 05 - Retrieve one of the entities using TableOperation.Retrieve and print its PartitionKey using Console.WriteLine()
+
+            // 06 - Retrieve all entities with PartitionKey "Netherlands" using TableQuery and print their RowKey using Console.WriteLine()
+
+            // 07 - Delete one of the entities from the table 
+
+            // 08 - Insert a new entity into "customers" using DynamicTableEntity instead of CustomerEntity
+        }
+    }
+}
